9a26c809a1974052353350cceebc00c0a9b0d0a7
Ruben Lukes <lukesruben@gmail.com>
2019-10-12 23:08:06 +0200

    added a todo model and nav menu

---
+++ Data/ToDoItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ToDoList_BlazorApp.Data
+{
+    public class ToDoItem
+    {
+        public String Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
